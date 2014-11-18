2812f68b8094ed3d7776539ad2fd92617e6ba9ae
Nick Boukis <poisongreekgyro@gmail.com>
2014-11-17 22:56:11 -0500

    Fixed

---
+++ testapp/src/com/example/testapp/TestClass.java
@@ -0,0 +1,13 @@
+package com.example.testapp;
+
+public class TestClass
+{
+	
+	public String goodbyefunc()
+	{
+		String greeting = "Goodbye boop!";
+		
+		return greeting;
+	}
+
+}
--- testapp/src/com/example/testapp/goodbye.java
+++
@@ -1,7 +0,0 @@
-public String goodbyefunc()
-{
-	String greeting = "Goodbye brah";
-	
-	return geeting;
-	
-}
