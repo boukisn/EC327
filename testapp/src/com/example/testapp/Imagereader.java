a69ec538cabe9d88f93b0c60b481f097e37089d1
ewayda2222 <ewayda@bu.edu>
2014-11-27 13:44:52 -0500

    displaying pixels and just getting -1

--- testapp/src/com/example/testapp/Imagereader.java
+++ testapp/src/com/example/testapp/Imagereader.java
@@ -24,27 +24,38 @@ public class Imagereader {
 		
 		bmp.getPixels(pixels, 0, width, 1, 1, width - 1, height - 1);
 		ArrayList<Integer> colors = new ArrayList<Integer>();
-		//Log.i("OutPut", "Colors: ");
+	
 		
-		for(int i =0; i < 100; i++)
+		for(int i =0, j = 0; i < 100; i++, j++)
 		{
-			if(colors.size() == 0)
+			
+			Log.d("Pixels", "Pixel is :"+pixels[i]);
+			
+			/*if(colors.size() == 0)
 				colors.add(pixels[i]);
 			
-			for(int j = 0; j < colors.size(); j++)
+			else if(pixels[i] != colors.get(j))
+			{
+				colors.add(pixels[i]);
+			}*/
+			
+			/*for(int j = 0; j < colors.size(); j++)
 			{
 				if(pixels[i] != colors.get(j))
 				{
 					colors.add(pixels[i]);
 				}
-			}
+			}*/
 		}
 		
 		//System.out.print("Colors: ");
 		Log.i("OutPut", "Colors: ");
 		Integer size = pixels.length;
+		Integer size1 = colors.size();
 		String f = size.toString();
-		Log.i("OutPut", f);
+		String f1 = size1.toString();
+		Log.i("Pixels", f);
+		Log.i("Colors", f1);
 		
 		/*for(int j = 0; j < colors.size(); j++)
 		{
