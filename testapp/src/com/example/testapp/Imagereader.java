9c9312898e1922a62449452c6b634164d0847d03
ewayda2222 <ewayda@bu.edu>
2014-11-27 14:56:29 -0500

    Clean up class part 2

--- testapp/src/com/example/testapp/Imagereader.java
+++ testapp/src/com/example/testapp/Imagereader.java
@@ -25,55 +25,33 @@ public class Imagereader {
 		bmp = BitmapFactory.decodeFile(file.getAbsolutePath().toString(), options);
 		height = bmp.getHeight();
 		width =  bmp.getWidth();
+	
 		if(bmp !=null)
 		{
 			pixels = new int[height*width];
 			
 			bmp.getPixels(pixels, 0, width, 0, 0, width , height );
-			ArrayList<Integer> colors = new ArrayList<Integer>();
+			//ArrayList<Integer> colors = new ArrayList<Integer>();
+		}
+	}
 		
-			
-			for(int i =0; i < pixels.length; i++)
-			{
-				if (pixels[i] != -1)
-				Log.d("Pixels", "Pixel is :"+pixels[i]);
-				
-				/*if(colors.size() == 0)
-					colors.add(pixels[i]);
-				
-				else if(pixels[i] != colors.get(j))
-				{
-					colors.add(pixels[i]);
-				}*/
-				
-				/*for(int j = 0; j < colors.size(); j++)
-				{
-					if(pixels[i] != colors.get(j))
-					{
-						colors.add(pixels[i]);
-					}
-				}*/
-			}
-			
-			//System.out.print("Colors: ");
-			Log.i("OutPut", "Colors: ");
-			Integer size = pixels.length;
-			Integer size1 = colors.size();
-			String f = size.toString();
-			String f1 = size1.toString();
-			Log.i("Pixels", f);
-			Log.i("Colors", f1);
-			
-			/*for(int j = 0; j < colors.size(); j++)
-			{
-					Integer msg = colors.get(j);
-					String str1= msg.toString();
-					Log.i("OutPut", str1 + ", ");
-					
-			}*/
+	public void output(){
 		
 	
+		for(int i =0; i < pixels.length; i++)
+		{
+			if (pixels[i] != -1)
+			Log.d("Pixels", "Pixel is :"+pixels[i]);
+			
 		}
-  
 	}
+		
+	
+			
+		
+		
+	
+		
+  
+
 }
