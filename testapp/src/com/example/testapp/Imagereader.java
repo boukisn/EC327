69e65ca59d020e73eee8ce47c63f9dcaedf1f773
ewayda2222 <ewayda@bu.edu>
2014-11-27 14:01:38 -0500

    We have colors!!!!!!!!!!!!

--- testapp/src/com/example/testapp/Imagereader.java
+++ testapp/src/com/example/testapp/Imagereader.java
@@ -14,59 +14,61 @@ public class Imagereader {
 	
 	public void foo(){
 		
-		int[] pixels;
+		int[] pixels = new int[1];
 		File file = new File("/storage/emulated/0/Download/ScheduleImageServlet.gif");
-		Bitmap bmp = BitmapFactory.decodeFile(file.getAbsolutePath().toString());
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		Bitmap bmp = BitmapFactory.decodeFile(file.getAbsolutePath().toString(), options);
 		int height = bmp.getHeight();
 		int width =  bmp.getWidth();
-		
-		pixels = new int[height*width];
-		
-		bmp.getPixels(pixels, 0, width, 1, 1, width - 1, height - 1);
-		ArrayList<Integer> colors = new ArrayList<Integer>();
-	
-		
-		for(int i =0, j = 0; i < 100; i++, j++)
+		if(bmp !=null)
 		{
+			pixels = new int[height*width];
 			
-			Log.d("Pixels", "Pixel is :"+pixels[i]);
+			bmp.getPixels(pixels, 0, width, 0, 0, width , height );
+			ArrayList<Integer> colors = new ArrayList<Integer>();
+		
 			
-			/*if(colors.size() == 0)
-				colors.add(pixels[i]);
-			
-			else if(pixels[i] != colors.get(j))
+			for(int i =0; i < pixels.length; i++)
 			{
-				colors.add(pixels[i]);
-			}*/
+				if (pixels[i] != -1)
+				Log.d("Pixels", "Pixel is :"+pixels[i]);
+				
+				/*if(colors.size() == 0)
+					colors.add(pixels[i]);
+				
+				else if(pixels[i] != colors.get(j))
+				{
+					colors.add(pixels[i]);
+				}*/
+				
+				/*for(int j = 0; j < colors.size(); j++)
+				{
+					if(pixels[i] != colors.get(j))
+					{
+						colors.add(pixels[i]);
+					}
+				}*/
+			}
+			
+			//System.out.print("Colors: ");
+			Log.i("OutPut", "Colors: ");
+			Integer size = pixels.length;
+			Integer size1 = colors.size();
+			String f = size.toString();
+			String f1 = size1.toString();
+			Log.i("Pixels", f);
+			Log.i("Colors", f1);
 			
 			/*for(int j = 0; j < colors.size(); j++)
 			{
-				if(pixels[i] != colors.get(j))
-				{
-					colors.add(pixels[i]);
-				}
+					Integer msg = colors.get(j);
+					String str1= msg.toString();
+					Log.i("OutPut", str1 + ", ");
+					
 			}*/
+		
+	
 		}
-		
-		//System.out.print("Colors: ");
-		Log.i("OutPut", "Colors: ");
-		Integer size = pixels.length;
-		Integer size1 = colors.size();
-		String f = size.toString();
-		String f1 = size1.toString();
-		Log.i("Pixels", f);
-		Log.i("Colors", f1);
-		
-		/*for(int j = 0; j < colors.size(); j++)
-		{
-				Integer msg = colors.get(j);
-				String str1= msg.toString();
-				Log.i("OutPut", str1 + ", ");
-				
-		}*/
-		
-	
-	
   
 	}
 }
