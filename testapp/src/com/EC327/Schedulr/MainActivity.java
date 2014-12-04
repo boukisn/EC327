c0c88f59f736e7f4f39c54312cdcddc82092da4a
Nick Boukis <poisongreekgyro@gmail.com>
2014-12-04 12:47:28 -0500

    added font

--- testapp/src/com/EC327/Schedulr/MainActivity.java
+++ testapp/src/com/EC327/Schedulr/MainActivity.java
@@ -4,10 +4,12 @@ import com.EC327.Schedulr.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -16,6 +18,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String fontPath = "OpenSans-Light.ttf";
+        TextView txtTitle = (TextView) findViewById(R.id.textView1);
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        txtTitle.setTypeface(tf);
+        
     }
 
 
